faf90168503932c443219a62a16b4d53a3797caa
Torishnia <valeri.torishnya@gmail.com>
2023-02-20 00:15:10 +0300

    Create component Header

---
+++ pizza-land/src/app/App.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header';
+import style from './app.module.sass';
+
+function App() {
+  return (
+    <div className={style.container}>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pizza-land/src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import pizzaSlice from '../image/pizzaSlice.png';
+import style from './header.module.sass';
+
+export default function Header() {
+  return (
+    <div className={style.header}>
+          <div className={style.container} >
+
+            <div className={style.header_logo}>
+
+              <div>
+                <img src={pizzaSlice} alt="logo" />
+              </div>
+
+              <div>
+                <h1>Pizza Land</h1>
+                <p>The most delicious pizza in the Universe</p>
+              </div>
+
+            </div>
+            
+            <div>
+              <button className={style.button_cart}>
+                <HiOutlineShoppingCart size={23}/>
+              </button>
+            </div>
+
+          </div>
+    </div>
+  )
+}
