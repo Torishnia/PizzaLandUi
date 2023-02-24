271b5c4f87f0c5286d96ff0571ff23cd7aac989c
Torishnia <valeri.torishnya@gmail.com>
2023-02-24 16:19:23 +0300

    Update styles

--- pizza-land/src/app/App.js
+++ pizza-land/src/app/App.js
@@ -2,16 +2,29 @@ import Categories from '../components/Categories/Categories';
 import Header from '../components/Header/Header';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import Sort from '../components/Sort/Sort';
+import PizzaSlider_1 from '../components/image/pizza_slider_1.png';
+import PizzaSlider_2 from '../components/image/pizza_slider_2.png';
+import PizzaSlider_3 from '../components/image/pizza_slider_3.png';
 import style from './app.module.sass';
 
 function App() {
   return (
     <div className={style.container}>
       <Header />
-      <Categories />
-      <Sort />
-      <h2>All pizzas</h2>
-      <PizzaBlock />
+      <div className={style.content}>
+        <div className={style.wrapper}>
+            <img src={PizzaSlider_1} alt='PizzaSlider'/>
+            <img src={PizzaSlider_2} alt='PizzaSlider'/>
+            <img src={PizzaSlider_3} alt='PizzaSlider'/>
+        </div>
+      </div>
+        
+      <div>
+        <Categories />
+        <Sort />
+        <h2>All pizzas</h2>
+        <PizzaBlock />
+      </div>
     </div>
   );
 }
--- pizza-land/src/components/Header/Header.jsx
+++ pizza-land/src/components/Header/Header.jsx
@@ -7,32 +7,29 @@ import style from './header.module.sass';
 export default function Header() {
   return (
     <div className={style.header}>
-          <div className={style.container} >
+      <div className={style.container} >
 
-            <div className={style.header_logo}>
+        <div className={style.header_logo}>
 
-              <div>
-                <img src={pizzaSlice} alt="logo" />
-              </div>
-
-              <div>
-                <h1>Pizza Land</h1>
-                <p>The most delicious pizza in the Universe</p>
-              </div>
-
-            </div>
-            
-            <div>
-
-              <button className={style.button_cart}>
-                <HiOutlineShoppingCart/>
-              </button>
-
-              <div className={style.numberCart}>1</div>
-
-            </div>
-
+          <div>
+            <img src={pizzaSlice} alt="logo" />
           </div>
+
+          <div>
+            <h1>Pizza Land</h1>
+            <p>The most delicious pizza in the Universe</p>
+          </div>
+
+        </div>
+        
+        <div className={style.header_cart}>
+          <div className={style.cart_block}>
+            <button><HiOutlineShoppingCart/></button>
+            <div className={style.numberCart}>1</div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
