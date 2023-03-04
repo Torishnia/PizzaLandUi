8d5c9037632d302ee35ea6310856d41528fb210a
Torishnia <valeri.torishnya@gmail.com>
2023-03-04 12:01:06 +0300

    Create page NotFound

--- pizza-land/src/pages/NotFound.jsx
+++ pizza-land/src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Oops from '../assets/image/page_not_found.jpg';
+import style from '../pages/not_found.module.sass';
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className={style.container}>
+      <img src={Oops} alt='Oops'/>
+    </div>
   )
 }
