8c242d38041a89f819789de5002721812a423ce9
Torishnia <valeri.torishnya@gmail.com>
2023-03-08 15:29:22 +0300

    Create mockAPI

--- pizza-land/src/components/Products/Products.jsx
+++ pizza-land/src/components/Products/Products.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 import PizzaBlock from '../PizzaBlock/PizzaBlock';
-import { pizzas } from '../../data';
 import style from './products.module.sass';
 
 export default function Products() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://64021c7d3779a86262658057.mockapi.io/items')
+    .then((res) => res.json())
+    .then((arr) => {
+      setItems(arr);
+    });
+  }, []);
+
   return (
     <div className={style.items}>
-      { pizzas.map((pizza) => 
-        <PizzaBlock key={pizzas.id} {...pizza} />
+      { items.map((item) => 
+        <PizzaBlock key={item.id} {...item} />
       ) }
     </div>
   )
