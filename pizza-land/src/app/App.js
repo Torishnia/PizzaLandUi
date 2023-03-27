f447e776102d29364181aab7971f69e7bc5573cf
Torishnia <valeri.torishnya@gmail.com>
2023-03-27 20:21:58 +0300

    Add Outlet

--- pizza-land/src/app/App.js
+++ pizza-land/src/app/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from '../components/Header/Header';
 import Cart from '../pages/Cart/Cart';
 import FullPizza from '../pages/FullPizza/FullPizza';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
-import styles from './app.module.sass';
+import MainLayout from '../layouts/MainLayout';
 
 function App() {
   return (
-      <div className={styles.container}>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/cart' element={<Cart />}/>
-          <Route path='/pizza/:id' element={<FullPizza />}/>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+          <Route path='' element={<Home />}/>
+          <Route path='cart' element={<Cart />}/>
+          <Route path='pizza/:id' element={<FullPizza />}/>
           <Route path='*' element={<NotFound />}/>
-        </Routes>
-      </div>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ pizza-land/src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Header from '../components/Header/Header';
+
+export default function MainLayout() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
