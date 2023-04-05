da2e9156742f35652c27ad2b95d1c45712e79847
Torishnia <valeri.torishnya@gmail.com>
2023-04-05 18:58:50 +0300

    Create component Loader

--- pizza-land/src/components/CartEmpty/CartEmpty.tsx
+++ pizza-land/src/components/CartEmpty/CartEmpty.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import Loader from '../Loader/Loader';
 
 import Empty_cart from '../../assets/image/empty_cart.jpg';
 import styles from './cartEmpty.module.sass';
+import { useState } from 'react';
 
 export default function CartEmpty() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 1500)
+
   return (
     <div className={styles.container}>
       <h2>Сart is empty</h2>
-      <img src={Empty_cart} alt='Empty cart'/>
+      { isLoading ? <Loader /> : <img src={Empty_cart} alt='Empty cart'/> }
       <p>To order a pizza, go to the home page</p>
       <Link to='/'>
         <button>&larr; Go back</button>
---
+++ pizza-land/src/components/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import styles from './loader.module.sass';
+
+export default function Loader() {
+  return (
+    <div className={styles.loader}></div>
+  )
+}
